b8d825b34589bb1e3dcbb94ef2187dd23d583215
mgmilton <matt.milton@gmail.com>
2018-04-05 15:30:27 -0600

    adds state and coordinates to location model [#156515803]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404225632) do
+ActiveRecord::Schema.define(version: 20180405212949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20180404225632) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.string "coordinates", default: [], array: true
+    t.string "state"
   end
 
   create_table "users", force: :cascade do |t|
