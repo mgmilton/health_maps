647ab89b61a05bcaace525c362070f12455c4c43
mgmilton <matt.milton@gmail.com>
2018-04-14 16:23:11 -0600

    resolves merge conflict[#156542200]

---
+++ spec/features/user_can_login_via_twitter_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe "As a registered user" do
+  describe "When I click the twitter link in the dropdown" do
+    it "logs me in and sends me to the user dashbord page" do
+
+      visit "/"
+      click_link "Login"
+      click_link "Twitter"
+      expect(current_path).to be("/auth/twitter")
+    end
+  end
+end
