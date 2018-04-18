85104c6f87c2ae3c420f6eccdac62bb3a6d0cee0
mgmilton <matt.milton@gmail.com>
2018-04-18 08:21:20 -0600

    removes excess controllers

--- spec/services/cdc_service_spec.rb
+++ spec/services/cdc_service_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe LocationConnection do
-  it "retruns collection of CDC data for a user's location" do
+  it "returns collection of CDC data for a user's location" do
     VCR.use_cassette("cdc service", :record => :new_episodes) do
       user = create(:user)
 
@@ -25,6 +25,19 @@ describe LocationConnection do
         percentage_of_population_with_high_bp = neighborhood_health_data.first[:arthritis_crudeprev]
         expect(neighborhood_health_data.length). to eq(16)
         expect(percentage_of_population_with_high_bp). to eq("23.6")
+    end
+  end
+end
+
+describe LocationConnection do
+  it "works for a big city" do
+    VCR.use_cassette("cdc service Big City", :record => :new_episodes) do
+      subject = LocationConnection.new("Houston")
+      subject.get_jsons
+      neighborhood_health_data = subject.get_jsons
+      percentage_of_population_with_arthritis = neighborhood_health_data.first[:arthritis_crudeprev]
+      expect(neighborhood_health_data.length). to eq(552)
+      expect(percentage_of_population_with_arthritis). to eq("23.8")
     end
   end
 end
